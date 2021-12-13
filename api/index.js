1c19dfe05e6073cf3ed434829c7fc9adf18d4bcc
Vincent <dvincdt@gmail.com>
2021-12-13 21:39:25 +0100

    fix: use async/await

--- api/index.js
+++ api/index.js
@@ -1,24 +1,28 @@
 const { google } = require('googleapis')
 
+const getData = async () => {
+  const scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  const jwt = new google.auth.JWT(
+    process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    null,
+    // replace the escaped newline characters (https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse)
+    process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    scopes,
+  )
+  const sheets = google.sheets({ version: 'v4', auth: jwt })
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
+  })
+
+  return response.data.values
+}
+
 module.exports = (req, res) => {
   try {
-    const scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    const jwt = new google.auth.JWT(
-      process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-      null,
-      // replace the escaped newline characters (https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse)
-      process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      scopes,
-    )
-    const sheets = google.sheets({ version: 'v4', auth: jwt })
+   const result = await getData()
 
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
-    })
-
-    const rows = response.data.values
-
-    res.status(200).json(rows)
+    res.status(200).json(result)
   } catch (error) {
     res.status(500).send(error)
   }
