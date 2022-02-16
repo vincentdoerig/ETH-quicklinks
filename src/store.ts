f46e01cb6f138a4db1c342d5793b83d614836c47
Vincent <dvincdt@gmail.com>
2022-02-16 20:50:31 +0100

    feat: improve settings design, add unique ids

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,8 @@
 import { defineStore } from 'pinia'
+import { nanoid } from 'nanoid'
 
 export interface Subject {
-  id: number; // TODO: change to uuid
+  id: string;
   name: string;
   lecturer: string;
   mainLink: string;
@@ -9,6 +10,7 @@ export interface Subject {
 }
 
 interface SecondaryLinks {
+  id: string,
   label: string;
   href: string;
 }
@@ -22,64 +24,72 @@ export const useLinkStore = defineStore('links', {
   state: (): State => ({
     subjects: [
       {
+        id: nanoid(),
         name: 'Algorithmen und Wahrschein­lichkeit',
-        id: 0,
         lecturer: 'Prof. Angelika Steger, Prof. Emo Welzl',
         mainLink: 'https://moodle-app2.let.ethz.ch/course/view.php?id=16841',
         links: [
           {
+            id: nanoid(),
             label: 'Moodle',
             href: 'https://moodle-app2.let.ethz.ch/course/view.php?id=16841',
           },
           {
+            id: nanoid(),
             label: 'VVZ',
             href: 'http://vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=157731&semkez=2022S&ansicht=LEHRVERANSTALTUNGEN&lang=de',
           },
         ],
       },
       {
+        id: nanoid(),
         name: 'Digital Design and Computer Architecture',
-        id: 1,
         lecturer: 'Prof. Onur Mutlu',
         mainLink: 'https://safari.ethz.ch/digitaltechnik/doku.php',
         links: [
           {
+            id: nanoid(),
             label: 'Moodle',
             href: 'https://moodle-app2.let.ethz.ch/course/view.php?id=16852',
           },
           {
+            id: nanoid(),
             label: 'YouTube',
             href: 'https://www.youtube.com/c/OnurMutluLectures/videos',
           },
           {
+            id: nanoid(),
             label: 'VVZ',
             href: 'http://vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=159117&semkez=2022S&ansicht=LEHRVERANSTALTUNGEN&lang=de',
           },
         ],
       },
       {
+        id: nanoid(),
         name: 'Analysis I',
-        id: 2,
         lecturer: 'Prof. Özlem Imamoglu',
         mainLink: 'https://metaphor.ethz.ch/x/2022/fs/401-0212-16L/',
         links: [
           {
+            id: nanoid(),
             label: 'VVZ',
             href: 'http://vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2022S&ansicht=LEHRVERANSTALTUNGEN&lerneinheitId=158644&lang=de',
           },
         ],
       },
       {
+        id: nanoid(),
         name: 'Parallele Programmierung',
-        id: 3,
         lecturer: 'Prof. Torsten Hoefler, Dr. Barbara Solenthaler',
         mainLink: 'https://spcl.inf.ethz.ch/Teaching/2022-pp/',
         links: [
           {
+            id: nanoid(),
             label: 'Moodle',
             href: 'https://moodle-app2.let.ethz.ch/course/view.php?id=16717',
           },
           {
+            id: nanoid(),
             label: 'VVZ',
             href: 'http://vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=157750&semkez=2022S&ansicht=LEHRVERANSTALTUNGEN&lang=de',
           },
@@ -105,13 +115,37 @@ export const useLinkStore = defineStore('links', {
       })
     },
 
-    removeSubject(id: number) {
+    removeSubject(id: string) {
       this.subjects = this.subjects.filter((subject) => subject.id !== id)
     },
 
     // add a subject last
     addSubject(subject: Subject) {
-      this.subjects = [...this.subjects, { ...subject,  id: Math.random() * 100 }]
+      this.subjects = [...this.subjects, { ...subject,  id: nanoid() }]
+    },
+
+    addLink(subjectId: string, data: SecondaryLinks) {
+      this.subjects = this.subjects.map((subject) => {
+        if (subject.id === subjectId) {
+          return {
+            ...subject,
+            links: [...subject.links, { ...data, id: nanoid() }],
+          }
+        }
+        return subject
+      })
+    },
+
+    removeLink(subjectId: string, linkId: string) {
+      this.subjects = this.subjects.map((subject) => {
+        if (subject.id === subjectId) {
+          return {
+            ...subject,
+            links: subject.links.filter((link) => link.id !== linkId),
+          }
+        }
+        return subject
+      })
     },
   },
   persist: true,
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,29 +1,78 @@
 <template>
   <!-- dark:bg-slate-900 -->
-  <div class="min-h-screen py-6 ">
+  <div class="min-h-screen">
     <main class="pb-8">
       <div class="flex-1 xl:overflow-y-auto">
-        <div class="max-w-3xl px-4 py-10 mx-auto sm:px-6 lg:py-12 lg:px-8">
-          <h1 class="text-3xl font-extrabold text-blue-gray-900">
-            Settings
-          </h1>
+        <div class="max-w-4xl px-4 py-10 mx-auto sm:px-6 lg:py-12 lg:px-8">
+          <div class="flex flex-col mb-8">
+            <h1 class="order-1 mt-2 text-3xl font-extrabold text-blue-gray-900">
+              Settings
+            </h1>
+            <NavBar />
+          </div>
 
-          <form class="mt-6 space-y-8 divide-y divide-y-blue-gray-200">
-            <div class="grid grid-cols-1 gap-y-6 sm:grid-cols-6 sm:gap-x-6">
-              <div class="sm:col-span-6">
+
+          <div class="hidden p-4 mt-4 bg-blue-900 rounded-md dark:block">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <MoonIcon
+                  class="w-5 h-5 text-blue-200"
+                  aria-hidden="true"
+                />
+              </div>
+              <div class="flex-1 ml-3 md:flex md:justify-between">
+                <p class="text-sm text-blue-100">
+                  Dark mode for the settings page coming soon™.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <form class="mt-6">
+            <div class="space-y-6">
+              <div>
                 <h2 class="text-xl font-medium text-blue-gray-900">
-                  General
+                  Subjects
                 </h2>
                 <p class="mt-1 text-sm text-blue-gray-500">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, officia. Vel illum laudantium ratione eos!
+                  Add, edit and delete subjects or reset them to the default (ETH BCS FS22 BPB2).
+                </p>
+              </div>
+
+              <!-- empty state -->
+              <div
+                v-if="subjects.length == 0"
+                class="text-center"
+              >
+                <svg
+                  class="w-12 h-12 mx-auto text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    vector-effect="non-scaling-stroke"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+                  />
+                </svg>
+                <h3 class="mt-2 text-sm font-medium text-gray-900">
+                  No subjects
+                </h3>
+                <p class="mt-1 text-sm text-gray-500">
+                  Get started by adding a new subject.
                 </p>
               </div>
 
               <div
                 v-for="subject in subjects"
                 :key="subject.id"
-                class="overflow-hidden bg-white rounded-lg shadow dark:bg-slate-800 sm:col-span-6"
+                class="overflow-hidden bg-white rounded-lg shadow"
               >
+                <!-- dark:bg-slate-800 -->
                 <div class="p-6">
                   <div class="relative -space-y-px rounded-md shadow-sm isolate">
                     <h3
@@ -34,13 +83,13 @@
                     </h3>
                     <h3
                       v-else
-                      class="mb-1 text-lg font-medium text-gray-500 cursor-not-allowed"
+                      class="mb-1 text-lg font-medium text-gray-500"
                     >
                       New Subject
                     </h3>
                     <button
                       type="button"
-                      class="absolute top-0 right-0 inline-flex items-center p-1 text-gray-700 bg-red-200 rounded hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                      class="absolute top-0 right-0 inline-flex items-center p-1 text-red-800 bg-red-100 border rounded border-red-50 hover:bg-red-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600 "
                       @click="deleteSubject(subject.id)"
                     >
                       <svg
@@ -57,127 +106,155 @@
                         />
                       </svg>
                     </button>
-                    <div class="relative px-3 py-2 border border-gray-300 rounded-md rounded-b-none focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                    <div class="relative px-3 py-2 border border-gray-300 rounded-md rounded-b-none focus-within:z-10 focus-within:ring-1 focus-within:ring-blue-600 focus-within:border-blue-600">
                       <label
-                        for="name-2"
+                        :for="`name-${subject.id}`"
                         class="block text-xs font-medium text-gray-700"
                       >Subject Name</label>
                       <input
-                        id="name-2"
+                        :id="`name-${subject.id}`"
                         v-model="subject.name"
                         type="text"
-                        class="block p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
+                        :name="`name-${subject.id}`"
+                        class="block w-full p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
                         placeholder="Analysis I"
                       >
                     </div>
-                    <div class="relative px-3 py-2 border border-gray-300 focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                    <div class="relative px-3 py-2 border border-gray-300 focus-within:z-10 focus-within:ring-1 focus-within:ring-blue-600 focus-within:border-blue-600">
                       <label
-                        for="lecturer"
+                        :for="`lecturer-${subject.id}`"
                         class="block w-full text-xs font-medium text-gray-700"
                       >Lecturer</label>
-                      <!-- todo: add unique id to input ids ('for' above also) -->
                       <input
-                        id="lecturer"
+                        :id="`lecturer-${subject.id}`"
                         v-model="subject.lecturer"
                         type="text"
-                        name="lecturer"
+                        :name="`lecturer-${subject.id}`"
                         class="block w-full p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
                         placeholder="Prof. Albert Einstein"
                       >
                     </div>
-                    <div class="relative px-3 py-2 border border-gray-300 rounded-md rounded-t-none focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                    <div class="relative px-3 py-2 border border-gray-300 rounded-md rounded-t-none focus-within:z-10 focus-within:ring-1 focus-within:ring-blue-600 focus-within:border-blue-600">
                       <label
-                        for="main-link"
+                        :for="`main-link-${subject.id}`"
                         class="block w-full text-xs font-medium text-gray-700"
                       >Main Link</label>
                       <input
-                        id="main-link"
+                        :id="`main-link-${subject.id}`"
                         v-model="subject.mainLink"
                         type="text"
-                        name="main-link"
+                        :name="`main-link-${subject.id}`"
                         class="block w-full p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
                         placeholder="https://dept.ethz.ch/20xx/xx"
                       >
                     </div>
                   </div>
 
-                  <p class="mt-1 text-sm text-blue-gray-500">
-                    Additional links
-                  </p>
-                  <div class="flex items-center justify-between mt-2">
-                    <div
-                      v-for="link in subject.links"
-                      :key="link.href"
-                    >
-                      <div class="-space-y-px rounded-md shadow-sm isolate">
-                        <div class="relative px-3 py-2 border border-gray-300 rounded-md rounded-b-none focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
-                          <label
-                            for="name"
-                            class="block text-xs font-medium text-gray-700"
-                          >Link Label</label>
-                          <input
-                            id="name"
-                            v-model="link.label"
-                            type="text"
-                            name="name"
-                            class="block p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
-                            placeholder="Script"
+                  <div class="ml-4">
+                    <p class="mt-3 text-sm font-medium text-gray-800">
+                      Link collection
+                    </p>
+                    <div class="grid gap-4 mt-2 md:grid-cols-2">
+                      <div
+                        v-for="link in subject.links"
+                        :key="link.id"
+                      >
+                        <div class="-space-y-px rounded-md shadow-sm isolate">
+                          <div class="relative px-3 py-2 border border-gray-300 rounded-md rounded-b-none focus-within:z-10 focus-within:ring-1 focus-within:ring-blue-600 focus-within:border-blue-600">
+                            <label
+                              :for="`label-${link.id}`"
+                              class="block text-xs font-medium text-gray-700"
+                            >Link Label</label>
+                            <input
+                              :id="`label-${link.id}`"
+                              v-model="link.label"
+                              type="text"
+                              :name="`label-${link.id}`"
+                              class="block w-full p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
+                              placeholder="Script"
+                            >
+                          </div>
+                          <div class="relative px-3 py-2 border border-gray-300 focus-within:z-10 focus-within:ring-1 focus-within:ring-blue-600 focus-within:border-blue-600">
+                            <label
+                              :for="`href-${link.id}`"
+                              class="block text-xs font-medium text-gray-700"
+                            >Link Destination</label>
+                            <input
+                              :id="`href-${link.id}`"
+                              v-model="link.href"
+                              type="text"
+                              :name="`href-${link.id}`"
+                              class="block w-full p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
+                              placeholder="https://xxx"
+                            >
+                          </div>
+
+                          <button
+                            type="button"
+                            class="relative inline-flex items-center w-full p-1 px-3 py-2 text-sm text-red-800 border border-gray-300 rounded-md rounded-t-none hover:bg-red-100 bg-red-50 focus-within:z-10 focus-within:ring-1 focus-within:ring-red-600 focus-within:border-red-600"
+                            @click="deleteLink(subject.id, link.id)"
                           >
-                        </div>
-                        <div class="relative px-3 py-2 border border-gray-300 rounded-md rounded-t-none focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
-                          <label
-                            for="job-title"
-                            class="block w-full text-xs font-medium text-gray-700"
-                          >Link Destination</label>
-                          <input
-                            id="job-title"
-                            v-model="link.href"
-                            type="text"
-                            name="job-title"
-                            class="block w-full p-0 text-gray-900 placeholder-gray-500 border-0 focus:ring-0 sm:text-sm"
-                            placeholder="https://xxx"
-                          >
+                            Delete Link
+                          </button>
                         </div>
                       </div>
+                      <button
+                        type="button"
+                        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 h-fit"
+                        @click="addNewLink(subject.id)"
+                      >
+                        <PlusSmIcon
+                          class="-ml-0.5 mr-2 h-4 w-4"
+                          aria-hidden="true"
+                        />
+                        New Link
+                      </button>
                     </div>
-                    <button
-                      type="button"
-                      class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      <PlusSmIconSolid
-                        class="-ml-0.5 mr-2 h-4 w-4"
-                        aria-hidden="true"
-                      />
-                      new link
-                    </button>
                   </div>
                 </div>
               </div>
             </div>
 
-            <button
-              type="button"
-              class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              @click="addNewSubject"
-            >
-              new subject
-            </button>
-          </form>
-        </div>
+            <div class="flex items-center mt-6">
+              <button
+                type="button"
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                @click="addNewSubject"
+              >
+                <PlusSmIcon
+                  class="-ml-0.5 mr-2 h-4 w-4"
+                  aria-hidden="true"
+                />
+                Add a New Subject
+              </button>
 
-        <div class="flex justify-end pt-8">
-          <button
-            type="button"
-            class="px-4 py-2 text-sm font-medium bg-white border border-gray-300 rounded-md shadow-sm text-blue-gray-900 hover:bg-blue-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          >
-            Save
-          </button>
+              <div class="ml-4 space-x-2">
+                <button
+                  type="button"
+                  class="inline-flex justify-center px-2.5 py-1.5 text-xs font-medium text-white bg-red-600 border border-transparent rounded shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                  @click="resetState"
+                >
+                  Reset to Original State
+                </button>
+                <button
+                  type="button"
+                  class="inline-flex justify-center px-2.5 py-1.5 text-xs font-medium text-white bg-red-600 border border-transparent rounded shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                  @click="deleteState"
+                >
+                  Clear State (Delete All)
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <div class="mt-6">
+            <router-link
+              class="text-sm font-medium text-gray-500 hover:text-gray-700"
+              to="/"
+            >
+              &larr; Return Home
+            </router-link>
+          </div>
         </div>
       </div>
     </main>
@@ -187,12 +264,17 @@
 <script lang="ts">
 import { storeToRefs } from 'pinia'
 import { defineComponent } from 'vue'
-import { PlusSmIcon as PlusSmIconSolid } from '@heroicons/vue/solid'
+import { nanoid } from 'nanoid'
+import { PlusSmIcon } from '@heroicons/vue/solid'
+import { MoonIcon } from '@heroicons/vue/outline'
 import { useLinkStore } from '../store'
+import NavBar from '../components/NavBar.vue'
 
 export default defineComponent({
   components: {
-    PlusSmIconSolid,
+    NavBar,
+    PlusSmIcon,
+    MoonIcon,
   },
 
   setup() {
@@ -202,24 +284,39 @@ export default defineComponent({
 
      const addNewSubject = () => {
       store.addSubject({
-        id: subjects.value.length, // TODO: change to uuid
+        id: nanoid(),
         name: '',
         lecturer: '',
         mainLink: '',
-        links: [
-          {
-            label: '',
-            href: '',
-          },
-        ],
+        links: [],
       })
     }
 
-    const deleteSubject = (id: number) => {
+    const addNewLink = (subjectId: string) => {
+      store.addLink(subjectId, {
+        id: nanoid(),
+        label: '',
+        href: '',
+      })
+    }
+
+    const deleteSubject = (id: string) => {
       store.removeSubject(id)
     }
 
-    return { subjects, addNewSubject, deleteSubject }
+    const deleteLink = (subjectId: string, linkId: string) => {
+      store.removeLink(subjectId, linkId)
+    }
+
+    const resetState = () => {
+      store.$reset()
+    }
+
+    const deleteState = () => {
+      store.$state = { subjects: [], filter: 'all' }
+    }
+
+    return { subjects, addNewSubject, deleteSubject, resetState, addNewLink, deleteLink, deleteState }
   },
 
 
