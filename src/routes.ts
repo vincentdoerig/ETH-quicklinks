35b6f52f6c407f3d0a1676c4b222cd1eed4f1afe
Vincent Dörig <dvincdt@gmail.com>
2022-09-17 13:19:20 +0200

    fix: default route for n.ethz

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import Settings from './views/Settings.vue'
 import NotFound from './views/NotFound.vue'
 
 export const routes = [
-  { path: '/', component: Home, meta: { title: 'Home' }},
+  { path: '', component: Home, meta: { title: 'Home' }},
   { path: '/settings', component: Settings, meta: { title: 'Settings' }},
   { path: '/:pathMatch(.*)*', component: NotFound, meta: { title: 'Page not found' }},
 ]
